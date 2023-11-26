024e4c5d34e4c01bcf50c06f78f3b8ae18bb8cf7
hiwa Taheri <hiwataheri@hiwas-MBP.fritz.box>
2023-11-26 15:12:19 +0100

    randomWord

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:wordpair_generator/utils/generateMaterialColor.dart';
+import 'package:wordpair_generator/utils/customTheme.dart';
+import 'package:wordpair_generator/random_words.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+            primarySwatch: generateMaterialColor(Palette.primaryColor)),
+        home: const RandomWords());
+  }
+}
